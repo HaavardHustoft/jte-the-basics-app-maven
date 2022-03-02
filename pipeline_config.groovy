fa20e5c841405966e7486ff613d399b2969cb065
Håvard Hustoft <haavard.h.97@gmail.com>
2022-03-02 15:58:37 +0100

    deploy_to

--- pipeline_config.groovy
+++ pipeline_config.groovy
@@ -9,3 +9,13 @@ stages{
         static_code_analysis
     }
 }
+
+application_environments{
+    dev{
+        ip_addresses = [ "0.0.0.1", "0.0.0.2" ]
+    }
+    prod{
+        long_name = "Production"
+        ip_addresses = [ "0.0.0.1", "0.0.1.2", "0.0.1.3", "0.0.1.4" ]
+    }
+}
